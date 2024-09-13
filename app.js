2d64e56f47f6da5b3311df45c7e9362d429b8fa3
maheeen-khan <maheeenkhan4@gmail.com>
2024-09-13 21:37:36 +0500

    First

---
+++ app.js
@@ -0,0 +1,34 @@
+// var arr1 = [1,2,3,4]
+// var arr2 = [11,12,13,14]
+// var len = 4;
+// var quantumSize = 2;
+
+// var count1 = 0;
+// var count2 = 0;
+
+// console.log("Start time of Array 1 : "+ count1)
+
+// for(var k = 0; k < quantumSize; k++ ){
+//     for(var i = 0; i < quantumSize; i++){
+//         console.log(arr1[count1]);
+//         console.log("Wait time "+ count1)
+//         // count1++
+//     }
+//     console.log("Arrival time of Array 1 : "+ count1)
+
+//     for(var j = 0; j < quantumSize; j++){
+//         console.log(arr2[count2]);
+//         count2++
+//     }
+   
+//     console.log("Arrival time of Array 2 : "+count2)
+// }
+
+
+// console.log("End time of Array 1 : "+ count1);
+
+// console.log("End time of Array 2 : "+count2);
+
+
+
+
